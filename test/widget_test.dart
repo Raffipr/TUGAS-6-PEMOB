36a6615a6ff7dff862786cb47f327fbf0b6dd25c
Raffipr <raffiprayoga269@gmail.com>
2024-12-18 18:01:58 +0700

    widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:tugas6_taufikurahman_mobile/main.dart';
+import 'package:tugas6_raffi/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
